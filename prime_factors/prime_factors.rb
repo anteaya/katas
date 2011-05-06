56509728243e5128c36a134cb952f82843486040
Anita Kuno <ay.kuno@gmail.com>
2011-05-06 16:44:29 -0400

    fixed formatting

--- prime_factors/prime_factors.rb
+++ prime_factors/prime_factors.rb
@@ -1,14 +1,14 @@
 class PrimeFactors
   def generate(number)
-	  prime_factors = []
-		potential_factor = 2
-		while number > 1
-			while number % potential_factor == 0
-				prime_factors << potential_factor
-				number = number / potential_factor
-			end
-			potential_factor += 1
-		end
-		prime_factors
-	end
+    prime_factors = []
+    potential_factor = 2
+    while number > 1
+      while number % potential_factor == 0
+        prime_factors << potential_factor
+        number = number / potential_factor
+      end
+      potential_factor += 1
+    end
+    prime_factors
+  end
 end
--- prime_factors/test_prime_factors.rb
+++ prime_factors/test_prime_factors.rb
@@ -3,38 +3,38 @@ require 'test-unit'
 
 class PrimeFactorsTest < Test::Unit::TestCase
   def setup
-	  @primes = PrimeFactors.new
-	end
+    @primes = PrimeFactors.new
+  end
 	
-	def test_generate_takes_an_argument_and_returns_a_list
-		assert_equal([], @primes.generate(0))
-	end
+  def test_generate_takes_an_argument_and_returns_a_list
+    assert_equal([], @primes.generate(0))
+  end
 	 	
-	def test_one
-		assert_equal([], @primes.generate(1))
-	end
+  def test_one
+    assert_equal([], @primes.generate(1))
+  end
 	
-	def test_two
-		assert_equal([2], @primes.generate(2))
-	end
+  def test_two
+    assert_equal([2], @primes.generate(2))
+  end
 	
-	def test_three
-		assert_equal([3], @primes.generate(3))
-	end
+  def test_three
+    assert_equal([3], @primes.generate(3))
+  end
 	
-	def test_four
-		assert_equal([2,2], @primes.generate(4))
-	end
+  def test_four
+    assert_equal([2,2], @primes.generate(4))
+  end
 	
-	def test_six
-		assert_equal([2,3], @primes.generate(6))
-	end
+  def test_six
+    assert_equal([2,3], @primes.generate(6))
+  end
 	
-	def test_eight
-		assert_equal([2,2,2], @primes.generate(8))
-	end
+  def test_eight
+    assert_equal([2,2,2], @primes.generate(8))
+  end
 	
-	def test_nine
-	  assert_equal([3,3,], @primes.generate(9))
-	end
-																																												end 	= 	end
+  def test_nine
+    assert_equal([3,3,], @primes.generate(9))
+  end
+end
