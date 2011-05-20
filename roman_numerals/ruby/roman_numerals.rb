15c9e10749257c313717151889e2e61145eee18d
Anita Kuno <ay.kuno@gmail.com>
2011-05-20 10:11:13 -0400

    roman numerals kata with spec and code

---
+++ roman_numerals/ruby/roman_numerals.rb
@@ -0,0 +1,24 @@
+class RomanNumerals
+  attr_accessor :integer_to_roman
+
+  def integer_to_roman
+    @integer_to_roman = {
+                        10 => "X",
+                        9  => "IX",
+                        5  => "V",
+                        4  => "IV",
+                        1  => "I"
+    }
+  end
+
+  def to_roman(integer)
+    result = ""
+    integer_to_roman.each_pair do |key, value|
+      while integer >= key
+        result += value
+        integer -= key
+      end
+    end
+    result
+  end
+end
---
+++ roman_numerals/ruby/roman_numerals_spec.rb
@@ -0,0 +1,33 @@
+require 'rspec'
+require File.join(File.dirname(__FILE__), 'roman_numerals')
+
+describe RomanNumerals do
+  before(:each) do
+    @roman_numerals = RomanNumerals.new
+  end
+
+  it "roman_numerals should be an instance of RomanNumerals" do
+    @roman_numerals.should be_an_instance_of(RomanNumerals)
+  end
+
+  context "#to_roman" do
+    it "should take an integer and return the corresponding roman numeral as a string" do
+      @roman_numerals.to_roman(1).should be_an_instance_of(String)
+      hash = {
+               1 => "I",
+               2 => "II",
+               3 => "III",
+               4 => "IV",
+               5 => "V",
+               6 => "VI",
+               7 => "VII",
+               8 => "VIII",
+               9 => "IX",
+               10 => "X",
+               20 => "XX"
+      }.each_pair do |key, value|
+        @roman_numerals.to_roman(key).should == value
+      end
+    end
+  end
+end
