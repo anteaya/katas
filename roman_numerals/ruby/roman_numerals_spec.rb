3fbc91e850607c15bf6e2e4db2d4d50e044f86e1
Anita Kuno <ay.kuno@gmail.com>
2011-05-21 13:23:54 -0400

    moved the require rspec code back to the first line

--- roman_numerals/ruby/roman_numerals_spec.rb
+++ roman_numerals/ruby/roman_numerals_spec.rb
@@ -1,5 +1,5 @@
-require File.join(File.dirname(__FILE__), 'roman_numerals')
 require 'rspec'
+require File.join(File.dirname(__FILE__), 'roman_numerals')
 
 describe RomanNumerals do
   before(:each) do
